4e81a090f89e416f162ed08eb9072587e9397fde
GaWoonie <wjdrkdns2@gmail.com>
2021-12-21 11:28:54 +0900

    Initial Commit
    Build

---
+++ src/main/java/com/example/spider_api/SpiderApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.spider_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpiderApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spider_api/SpiderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spider_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
